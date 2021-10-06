290f22a3e5bb6eb6da262680b63022b9b268b64a
Rafael JPD <rafael.pezzuto@gmail.com>
2021-10-06 13:46:12 -0300

    remove connects desnecessários

--- dsm/ingress.py
+++ dsm/ingress.py
@@ -21,10 +21,7 @@ _docs_manager = DocsManager(_files_storage, _db_url, _v3_manager)
 _docs_manager.db_connect()
 
 _journals_manager = JournalsManager(_db_url)
-_journals_manager.db_connect()
-
 _issues_manager = IssuesManager(_db_url)
-_issues_manager.db_connect()
 
 
 def get_package_uri_by_pid(scielo_pid_v3):
